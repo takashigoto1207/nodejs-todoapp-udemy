43efc2e1a31b262f9216cd53c228c283e60606db
takashigoto1207 <takashigoto1207@gmail.com>
2023-03-08 23:26:47 +0900

    fix

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # nodejs-todoapp-udemy
+
+# udemy
+https://www.udemy.com/course/nodejs-mongodb-todoapp/
+
+# how to start
+```
+npm run dev
+```
--- app.js
+++ app.js
@@ -14,8 +14,8 @@ app.use("/api/v1/tasks", taskRoute);
 // データベースと接続
 const start = async () => {
   try {
-    await connectDB(process.env.MONGO_URL);
-    app.listen(PORT, console.log("サーバーが起動しました"));
+    await connectDB(process.env.MONGO_HEROKU_URL || process.env.MONGO_URL);
+    app.listen(process.env.PORT || PORT, console.log("サーバーが起動しました"));
   } catch (err) {
     console.log(err);
   }
